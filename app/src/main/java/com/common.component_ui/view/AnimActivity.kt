c51174080ca9188423306f502826692f84cd6b75
hushendian <hushendian@dongyin.net.cn>
2019-11-14 17:51:38 +0800

    提交Activity动画页面

--- app/src/main/java/com/common.component_ui/view/AnimActivity.kt
+++ app/src/main/java/com/common.component_ui/view/AnimActivity.kt
@@ -1,6 +1,7 @@
 package com.common.component_ui.view
 
 import com.domui.uicomponent.R
+import kotlinx.android.synthetic.main.activity_transition_anim.*
 
 class AnimActivity : BaseActivity() {
 
@@ -12,6 +13,14 @@ class AnimActivity : BaseActivity() {
     override fun initData() {
         showBack()
         showTitle(resources.getString(R.string.animation))
+        go.setOnClickListener({
+            finish()
+        })
 
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.push_bottom_in, R.anim.push_bottom_out)
     }
 }
